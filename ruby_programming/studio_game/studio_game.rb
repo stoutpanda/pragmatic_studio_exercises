6cabd4b5a403c570abb65a610a3ada2a2afa26e9
stoutpanda <jholder29@gmail.com>
2015-12-29 22:17:34 -0600

    "Lesson 2: Numbers & Strings"

--- ruby_programming/studio_game/studio_game.rb
+++ ruby_programming/studio_game/studio_game.rb
@@ -1,5 +1,16 @@
-greeting = "Welome!"
-3.times do
-  puts greeting.upcase
-end
-  puts Time.now
+#numbers & intergers - lesson 2
+player_1_name = "larry"
+player_1_health = 60
+player_2_name = "curly"
+player_2_health = 60
+player_3_name = "moe"
+player_3_health = 60
+
+puts player_1_name + '\'s health is ' + player_1_health.to_s
+puts "#{player_1_name}'s health is #{player_1_health}"
+puts "#{player_1_name}'s health is #{player_1_health * 3}"
+puts "#{player_1_name}'s health is #{player_1_health / 9}"
+puts "#{player_1_name}'s health is #{player_1_health / 9.0}"
+
+
+puts "Players: \n\t #{player_1_name} \n\t #{player_2_name} \n\t #{player_3_name}"
