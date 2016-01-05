562e11a4d16692120d2720562a8ca9f7a0ce8fd2
stoutpanda <jholder29@gmail.com>
2016-01-04 22:58:52 -0600

    Lesson 3 completed.

--- ruby_programming/studio_game/studio_game.rb
+++ ruby_programming/studio_game/studio_game.rb
@@ -1,16 +1,21 @@
-#numbers & intergers - lesson 2
+
+#Strings & Variables - lesson 3
+require 'time'
+
 player_1_name = "larry"
 player_1_health = 60
 player_2_name = "curly"
 player_2_health = 60
 player_3_name = "moe"
 player_3_health = 60
+player_4_name = "shemp"
+player_4_health = 90
+current_time = Time.new
 
-puts player_1_name + '\'s health is ' + player_1_health.to_s
-puts "#{player_1_name}'s health is #{player_1_health}"
-puts "#{player_1_name}'s health is #{player_1_health * 3}"
-puts "#{player_1_name}'s health is #{player_1_health / 9}"
-puts "#{player_1_name}'s health is #{player_1_health / 9.0}"
+puts player_1_name.capitalize + '\'s health is ' + player_1_health.to_s
+puts player_2_name.upcase + '\'s health is ' + player_2_health.to_s
+puts "#{player_3_name.capitalize}'s health is #{player_3_health.to_s}".center(50,"*")
 
+puts "#{player_4_name.capitalize.ljust(50,'.')} #{player_4_health.to_s} health"
 
-puts "Players: \n\t #{player_1_name} \n\t #{player_2_name} \n\t #{player_3_name}"
+puts "The game started on #{current_time.strftime ("%A %m/%d/%Y at %H:%M%P")}."
