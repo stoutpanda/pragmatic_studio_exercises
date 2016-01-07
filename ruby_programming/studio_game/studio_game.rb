ed38bc2f81cd120be891da971f6dd64c137325be
stoutpanda <jholder29@gmail.com>
2016-01-06 21:53:40 -0600

    Methods

--- ruby_programming/studio_game/studio_game.rb
+++ ruby_programming/studio_game/studio_game.rb
@@ -1,21 +1,13 @@
+#Methods - lesson 5
 
-#Strings & Variables - lesson 3
-require 'time'
+def say_hello (name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
+def time
+  Time.new.strftime("%H:%M:%S")
+end
 
-player_1_name = "larry"
-player_1_health = 60
-player_2_name = "curly"
-player_2_health = 60
-player_3_name = "moe"
-player_3_health = 60
-player_4_name = "shemp"
-player_4_health = 90
-current_time = Time.new
-
-puts player_1_name.capitalize + '\'s health is ' + player_1_health.to_s
-puts player_2_name.upcase + '\'s health is ' + player_2_health.to_s
-puts "#{player_3_name.capitalize}'s health is #{player_3_health.to_s}".center(50,"*")
-
-puts "#{player_4_name.capitalize.ljust(50,'.')} #{player_4_health.to_s} health"
-
-puts "The game started on #{current_time.strftime ("%A %m/%d/%Y at %H:%M%P")}."
+puts say_hello("larry",60)
+puts say_hello("moe")
+puts say_hello("curly",125)
+puts say_hello("shremp",90)
