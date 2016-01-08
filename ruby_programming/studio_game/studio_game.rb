b079781ea7d2dbd57546c4fb2db16754b398a113
StoutPanda <jason@notacc.net>
2016-01-07 22:39:39 -0600

    Chapter 6 classes

--- ruby_programming/studio_game/studio_game.rb
+++ ruby_programming/studio_game/studio_game.rb
@@ -1,13 +1,36 @@
-#Methods - lesson 5
+#Classes - Lesson 6
 
-def say_hello (name, health=100)
-  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
-end
-def time
-  Time.new.strftime("%H:%M:%S")
+class Player
+  def initialize (name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def to_s
+    "I'm #{@name} with a health of #{@health}!"
+  end
+
+
+  def w00t
+    @health += 15
+    puts "#{@name} got wooted!"
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got balmmed!"
+  end
+
 end
 
-puts say_hello("larry",60)
-puts say_hello("moe")
-puts say_hello("curly",125)
-puts say_hello("shremp",90)
+
+player1 = Player.new('larry',60)
+puts player1
+player2 = Player.new('moe')
+puts player2
+player3 = Player.new("curly", 125)
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
