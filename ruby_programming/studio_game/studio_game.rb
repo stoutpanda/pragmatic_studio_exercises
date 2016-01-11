051a5f28a349b2d4185220f6d49da336f2b5ca25
StoutPanda <jason@notacc.net>
2016-01-10 21:44:29 -0600

    Lesson 8, Attributes

--- ruby_programming/studio_game/studio_game.rb
+++ ruby_programming/studio_game/studio_game.rb
@@ -1,36 +1,43 @@
-#Classes - Lesson 6
+#Attributes - Lesson 7
 
 class Player
+  attr_reader :health #getter method
+  attr_accessor :name # getter & setter
+
   def initialize (name, health=100)
     @name = name.capitalize
     @health = health
   end
-
   def to_s
-    "I'm #{@name} with a health of #{@health}!"
+    "I'm #{@name} with a health of #{score}!"
   end
-
-
   def w00t
     @health += 15
     puts "#{@name} got wooted!"
   end
-
   def blam
     @health -= 10
     puts "#{@name} got balmmed!"
+  end
+  def score
+    @health + @name.length
+  end
+  def name
+    @name = @name.capitalize
   end
 
 end
 
 
 player1 = Player.new('larry',60)
-puts player1
 player2 = Player.new('moe')
-puts player2
 player3 = Player.new("curly", 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
+
+
+puts player1.health
+puts player2.health
+puts player3.health
+
+player2.name = "lawrence"
+puts player2.name
+puts player2.to_s
