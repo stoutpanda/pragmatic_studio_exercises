954036f1ebf95e44b11c53fe288889f9b01c195a
StoutPanda <jason@notacc.net>
2016-01-24 14:30:23 -0600

    Enumerable 1

---
+++ blocks_course/flyer-status.rb
@@ -0,0 +1,36 @@
+#block basics
+class Flyer
+  attr_reader :name, :email, :miles_flown
+  attr_accessor :status
+
+  def initialize(name, email, miles_flown, status=:bronze)
+    @name = name
+    @email = email
+    @miles_flown = miles_flown
+    @status = status
+  end
+
+  def to_s
+    "#{name} (#{email}): #{miles_flown} - #{status}"
+  end
+end
+
+
+
+flyers = []
+flyers << Flyer.new("Larry", "larry@example.com", 4000, :platinum)
+flyers << Flyer.new("Moe", "moe@example.com", 1000)
+flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
+flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
+
+frequent_flyers = flyers.select { |f| f.miles_flown >= 3000 }
+puts frequent_flyers
+
+infrequent_flyers = flyers.reject { |f| f.miles_flown >= 3000 }
+puts infrequent_flyers
+
+platinum_status = flyers.select { |f| f.status == :platinum}
+puts platinum_status
+
+bronze_first = flyers.detect { |f| f.status == :bronze}
+puts bronze_first
